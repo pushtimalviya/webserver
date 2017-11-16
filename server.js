215adbb8c63408d80f295878dfaf1647f91a9560
pushti <pushti.malviya@gmail.com>
2017-11-16 17:15:04 +0530

    server chnge

--- server.js
+++ server.js
@@ -1,27 +1,15 @@
 var express = require('express');
 var app = express();
 var PORT = 3000;
+var middleware = require('./midWare.js');
 
-var middleware = {
-	requiredAuth : function( req,res, next) {
-		console.log('secure route');
-		next();
-	},
-
-	logger: function (req,res,next) {
-
-		var str = new Date().toString();
-		console.log('requestd url'+ str+req.method+'  '+ req.originalUrl);
-		next();
-	}
-}
 
 	 app.use(middleware.logger);
 
  // app.use(middleware.requiredAuth);
 
  app.get( '/', function ( req,res) { 
- 			res.send('helloooo..');
+ 			res.send('helloooo..'); 
  });
 
  app.get('/about',middleware.requiredAuth, function ( req, res) {
